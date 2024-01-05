7b3dee9f8a59ea2f894b2e877917450f69ec41b9
Moonchichiii <>
2024-01-05 22:22:22 +0000

    imported reg/login modal, for setting up registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
+import React, { useState } from 'react';
+
+import Example from './components/Modal/AuthModal'; 
+import OffcanvasExample from './components/NavBar/NavBar'
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+
+
+
 function App() {
+  const [show, setShow] = useState(false);
 
-
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
 
   return (
-   
-      <div className="App">
-     
-        <h1>test!</h1>
-      </div>
-   
+    <div className="App">
+        <OffcanvasExample />
+      <h1>Recipe Respository</h1>
+      <Button variant="primary" onClick={handleShow}>
+        Accounts
+      </Button>
+      <Example show={show} handleClose={handleClose} />
+    </div>
   );
 }
 
