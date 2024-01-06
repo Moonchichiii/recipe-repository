1c3bed4b7a87f4cecccafe2ffad4fd6e6ac73f63
Moonchichiii <>
2024-01-06 13:08:42 +0000

    moved the forms inside the modal, next test social account sign in in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AuthModal from './components/Modal/AuthModal'; 
+import Button from 'react-bootstrap/Button';
 import { AuthProvider } from './components/Auth/Contexts/AuthContext';
-import LoginForm from './components/Auth/LoginForm/LoginForm';
-import RegistrationForm from './components/Auth/RegistrationForm/RegistrationForm';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-    return (
-        <AuthProvider>
-            <>
-                <LoginForm />
-                <RegistrationForm />
-            </>
-        </AuthProvider>
-    );
+  
+    const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+
+  return (
+<>
+<AuthProvider>
+
+
+        <Button variant="primary" onClick={handleShowModal}>
+
+          Accounts
+
+        </Button>
+
+        <AuthModal show={showModal} handleClose={handleCloseModal} />
+
+    </AuthProvider>
+    </>
+          );
 }
 
 export default App;
---
+++ src/components/Modal/AuthModal.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import LoginForm from '../Auth/LoginForm/LoginForm';  
+import RegistrationForm from '../Auth/RegistrationForm/RegistrationForm';
+
+function AuthModal({ show, handleClose }) {
+  const [isLoginActive, setIsLoginActive] = useState(true);
+
+  const toggleForm = () => setIsLoginActive(!isLoginActive);
+
+  return (
+    <Modal
+      show={show}
+      onHide={handleClose}
+      backdrop="static"
+      keyboard={false}
+      centered 
+    >
+      <Modal.Header closeButton>
+        <Modal.Title className="reg-header">
+          {isLoginActive ? 'Login' : 'Register'}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {isLoginActive ? (
+          <>
+            <LoginForm />
+            <div className="mt-3 text-center">
+              Don't have an account? 
+              <Button variant="link" onClick={toggleForm}>Sign up</Button>
+            </div>
+          </>
+        ) : (
+          <>
+            <RegistrationForm />
+            <div className="mt-3 text-center">
+              Have an account? 
+              <Button variant="link" onClick={toggleForm}>Login</Button>
+            </div>
+          </>
+        )}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default AuthModal;
