c86a0a5867069195e0734f6aac3d7a107e9280e9
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-01-09 00:20:37 +0100

    trying out vscode

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
