a971b2d9596ef0c57cf59a044f07a5615ef2d178
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-01-10 16:58:54 +0100

    Reverted to working  App.

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,16 @@ import ProtectedRoute from "./components/Auth/Protectedroute/ProtectedRoute";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-const LandingPage = () => (
-  <div>
-    <h1>Recipe Repository</h1>
-  </div>
-);
+const LandingPage = () => <div><h1>Recipe Repository</h1></div>;
 const AuthModal = lazy(() => import("./components/common/Modal/AuthModal"));
 const Dashboard = lazy(() => import("./components/pages/DashBoard/DashBoard"));
-const ProfileSetup = lazy(() =>
-  import("./components/Auth/Forms/ProfileSetup/ProfileSetup")
-);
+const ProfileSetup = lazy(() => import("./components/Auth/Forms/ProfileSetup/ProfileSetup"));
 
 function App() {
   const [showModal, setShowModal] = useState(false);
   const { isAuthenticated, handleLogout } = useContext(AuthContext);
 
-  return (
+  return (    
     <Router>
       {!isAuthenticated && (
         <Button variant="primary" onClick={() => setShowModal(true)}>
@@ -36,9 +30,7 @@ function App() {
       )}
 
       <Suspense fallback={<div>Loading...</div>}>
-        {showModal && (
-          <AuthModal show={showModal} handleClose={() => setShowModal(false)} />
-        )}
+        {showModal && <AuthModal show={showModal} handleClose={() => setShowModal(false)} />}
       </Suspense>
 
       <Routes>
