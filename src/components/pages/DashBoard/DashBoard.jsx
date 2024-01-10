a2ff355500ee8c379e2e75e9932fd1ec4b6ade86
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-01-10 05:13:12 +0100

    Refactor Dashboard component and moved Layout component

--- src/components/pages/DashBoard/DashBoard.jsx
+++ src/components/pages/DashBoard/DashBoard.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+// Dashboard component
+import React, { useContext } from 'react';
+import { Button } from 'react-bootstrap';
+import { AuthContext } from '../../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { logout } from '../../../service/Api';
 
 function Dashboard() {
+    const { setUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout().then(() => {
+
+            setUser(null);
+            localStorage.removeItem('token');
+            
+
+            navigate('/');
+        }).catch((error) => {
+            console.error('Logout failed:', error);
+        });
+    };
+
     return (
         <div>
             <h1>Dashboard</h1>
-            
-            <p>dashboard component as the navbar for the top . </p>
+            <Button onClick={handleLogout}>Logout</Button>
         </div>
     );
 }
--- src/styles/layout/LayOut.jsx
+++ src/styles/layout/LayOut.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NavBar from '../Navbar/NavBar';
 import AuthNavbar from '../../components/Authentication/AuthNavbar/AuthNavbar';
 
-import Footer from '../../common/Footer/Footer';
+import Footer from '../../components/common/Footer/Footer';
 
 const Layout = ({ children }) => (
   <>
