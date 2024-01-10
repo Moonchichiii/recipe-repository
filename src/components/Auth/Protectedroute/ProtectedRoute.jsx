6457c7eb53f5ee6218989c662ecb9b28203db818
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-01-10 14:18:04 +0100

    Refactor protectedroute component use of  navigate and Outlet from react.

--- src/components/Auth/Protectedroute/ProtectedRoute.jsx
+++ src/components/Auth/Protectedroute/ProtectedRoute.jsx
@@ -1,22 +1,11 @@
-import React, { useContext } from 'react'; 
-import { Route, Redirect } from 'react-router-dom';
+import { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 import { AuthContext } from '../../../context/AuthContext';
 
-const ProtectedRoute = ({ component: Component, ...rest }) => {
-    const { isAuthenticated } = useContext(AuthContext);
+const ProtectedRoute = () => {
+  const { isAuthenticated } = useContext(AuthContext);
 
-    return (
-        <Route
-            {...rest}
-            render={(props) => 
-                isAuthenticated ? (
-                    <Component {...props} />
-                ) : (
-                    <Redirect to="/login" />
-                )
-            }
-        />
-    );
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
