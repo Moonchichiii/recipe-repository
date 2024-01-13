62c6bd317b7dca695b29e899c3b8f63ef322a6da
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-01-13 17:44:10 +0100

    Updated layout import in App and DashBoard

--- src/components/pages/DashBoard/DashBoard.jsx
+++ src/components/pages/DashBoard/DashBoard.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import { useNavigate } from "react-router-dom";
-import Layout from "../../common/LayOut/LayOut";
+import Layout from "../../common/MainLayout/LayOut";
 
 
 
